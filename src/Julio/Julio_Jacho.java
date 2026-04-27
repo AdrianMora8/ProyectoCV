01d2dff3baf52870e79f6c5828c146c6911ca388
Adrian <oriofrio0126@gmail.com>
2026-04-27 17:52:11 -0500

    f3rew

--- src/Julio/Julio_Jacho.java
+++ src/Julio/Julio_Jacho.java
@@ -12,7 +12,5 @@ public class Julio_Jacho {
     public void nombre()
     {
         System.out.println("julio jacho");
-        
-        System.out.println("Julio.Julio_Jacho.nombre()");
     }
 }
